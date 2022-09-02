cc26e158e44148499b5ca2686dffd68d0e37c7c4
JuanParrado19 <juanaparrado19@gmail.com>
2022-09-02 10:26:07 -0500

    multi

--- src/co/edu/unisabana/example/Principal.java
+++ src/co/edu/unisabana/example/Principal.java
@@ -3,5 +3,7 @@ package co.edu.unisabana.example;
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        multiplicacion.multiplicacion();
     }
 }
+
