13be8986381b09a6411cb1c38daf929df41a74ff
MajuGiraldo <mariagina@unisabana.edu.co>
2022-09-02 11:17:51 -0500

    Funcionalidad suma

--- src/co/edu/unisabana/example/Principal.java
+++ src/co/edu/unisabana/example/Principal.java
@@ -2,14 +2,10 @@ package co.edu.unisabana.example;
 
 public class Principal {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Esta es la funcionalidad de suma");
-        Suma.suma();
-=======
         System.out.println("Hola soy pau");
+        Suma.suma();
         Division.division();
         Resta.resta();
->>>>>>> 0b8b6df40b56f14cd5a71177c31a793a5037d1da
     }
 
 }
