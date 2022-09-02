c1b66195e87a70f4270447df39c49bf58b477b7b
MajuGiraldo <mariagina@unisabana.edu.co>
2022-09-02 10:29:16 -0500

    Funcionalidad de suma

--- src/co/edu/unisabana/example/Principal.java
+++ src/co/edu/unisabana/example/Principal.java
@@ -2,6 +2,8 @@ package co.edu.unisabana.example;
 
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Esta es la funcionalidad de suma");
+        Division.division();
     }
+
 }
---
+++ src/co/edu/unisabana/example/Suma.java
@@ -0,0 +1,22 @@
+package co.edu.unisabana.example;
+Scanner reader = new Scanner(System.in);
+
+
+public class Suma{
+    public static void suma(){
+        int numero1 = 0;
+        int numero2 = 0;
+  
+        System.out.println("Introduce el primer número:");      
+        numero1 = reader.nextInt();
+      
+        System.out.println("Introduce el segundo número:");
+        numero2 = reader.nextInt();
+
+        resultado = numero1+numero2;
+
+        System.out.println("La suma es " + numero1 + " + " + numero2 + " = " + resultado);
+    }
+}
+
+
