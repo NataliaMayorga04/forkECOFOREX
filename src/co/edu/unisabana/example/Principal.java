406acf3f3afac136049173cd6f381ac8d506e059
MariaPaulaAcero <mariaacre@unisabana.edu.co>
2022-09-02 10:25:20 -0500

    division

--- src/co/edu/unisabana/example/Principal.java
+++ src/co/edu/unisabana/example/Principal.java
@@ -2,6 +2,7 @@ package co.edu.unisabana.example;
 
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola soy pau");
+        Division.division();
     }
 }
